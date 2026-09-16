b5f348d3af61f5e9b2f6c9a80547c426310de5ec
Rain Zhang <tza103@sfu.ca>
2026-09-16 12:32:05 -0700

    Print CLI errors as messages instead of Debug output

--- pc-hid-runner/src/main.rs
+++ pc-hid-runner/src/main.rs
@@ -1,3 +1,11 @@
-fn main() -> std::io::Result<()> {
-    pc_hid_runner::cli::run_cli()
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    match pc_hid_runner::cli::run_cli() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("pc-hid-runner: {err}");
+            ExitCode::FAILURE
+        }
+    }
 }
